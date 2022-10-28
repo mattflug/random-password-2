12db20139c19dbad51bc26759c03e8d6685ccb43
Ghost <mbflug@gmail.com>
2022-10-28 16:43:51 -0600

    updating variables

--- script.js
+++ script.js
@@ -1,4 +1,63 @@
 // Assignment Code
+// Upper case letters
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+// Lower case letters
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -7,8 +66,18 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  // Ask for user choice
+  var userChoice = window.prompt(
+    "Please enter a password length at least 8 and no more than 128 characters."
+  );
+  // If user pressed Cancel, immediately end function
+  if (!userChoice) {
+    return;
+  }
+}
